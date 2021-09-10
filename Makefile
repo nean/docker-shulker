8a8efecdae0dedfd3bef4b30a1ffedcc033bd3ab
Neranjen <mvneranjen@gmail.com>
2021-09-10 08:59:33 +0530

    update docker image for upstream changes

---
+++ Makefile
@@ -0,0 +1,5 @@
+build:
+	docker build . -t nean12/shulker:latest
+
+push:
+	docker push nean12/shulker:latest
--- entry-point.sh
+++ entry-point.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 
-false | cp -i /opt/shulker/config.json /shulker-config/config.json 2>/dev/null
+false | cp -i /opt/shulker/config.example.json /shulker-config/config.json 2>/dev/null
 
 node /opt/shulker/build/index.js /shulker-config/config.json
